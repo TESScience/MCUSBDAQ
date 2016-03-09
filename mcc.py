af86209ac5f250fba09bab209c81580f9673f025
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-03-09 16:46:19 -0500

    - Fixing some errors in Exceptions - Renaming '.h' attribute to '._hid_device' - Letting PyCharm reformat

--- mcc.py
+++ mcc.py
@@ -23,88 +23,89 @@ import struct
 import hid
 from mccusbtemp import *
 
+
 class usb_temp(object):
-    def __init__(self, serial_no = None):
-        self.h = hid.device()
+    def __init__(self, serial_no=None):
+        self._hid_device = hid.device()
         if serial_no is None:
-            self.h.open(VENDOR_ID, PRODUCT_ID, serial_no)
+            self._hid_device.open(VENDOR_ID, PRODUCT_ID, serial_no)
         else:
             found = False
             dev_list = hid.enumerate(VENDOR_ID, PRODUCT_ID)
             for test_dev in dev_list:
                 if serial_no == test_dev['serial_number']:
                     found = True
-                    self.h.open_path(test_dev['path'])
+                    self._hid_device.open_path(test_dev['path'])
                     break
             if not found:
                 raise ValueError("Invalid serial number")
 
     def __del__(self):
-        self.h.close()
+        self._hid_device.close()
 
     def _waitWhileBusy(self):
         while self.getStatus() != 0:
             pass
 
-    def _readData(self, report_id, length = 129, timeout = 500):
+    def _readData(self, report_id, length=129, timeout=500):
         try:
-            d = self.h.read(length, timeout)
-        except(IOError, ex):
+            d = self._hid_device.read(length, timeout)
+        except IOError:
             pass
         return d
 
     def getStatus(self):
         result = None
-        self.h.write([GET_STATUS, 0, 0])
+        self._hid_device.write([GET_STATUS, 0, 0])
         d = self._readData(GET_STATUS)
         if d:
             reportId = d[0]
-            result   = d[1]
+            result = d[1]
         return result
 
     def calibrate(self):
-        self.h.write([CALIBRATE])
+        self._hid_device.write([CALIBRATE])
         self._waitWhileBusy()
 
     def flashLED(self):
-        self.h.write([BLINK_LED, 0, 0])
+        self._hid_device.write([BLINK_LED, 0, 0])
 
     def reset(self):
-        self.h.write([RESET, 0, 0])
+        self._hid_device.write([RESET, 0, 0])
 
     def dConfigPort(self, direction):
-        self.h.write([DCONFIG, direction])
+        self._hid_device.write([DCONFIG, direction])
 
     def dConfigBit(self, bit_num, direction):
-        self.h.write([DCONFIG_BIT, bit_num, direction])
+        self._hid_device.write([DCONFIG_BIT, bit_num, direction])
 
     def dinPort(self):
         result = None
-        self.h.write([DIN, 0, 0])
+        self._hid_device.write([DIN, 0, 0])
         d = self._readData(DIN, 2)
         if d:
             reportId = d[0]
-            result   = d[1]
+            result = d[1]
         return result
 
     def dinBit(self):
         result = None
-        self.h.write([DBIT_IN])
+        self._hid_device.write([DBIT_IN])
         d = self._readData(DBIT_IN, 2)
         if d:
             reportId = d[0]
-            result   = d[1]
+            result = d[1]
         return result
 
     def doutPort(self, value):
-        self.h.write([DOUT, value])
+        self._hid_device.write([DOUT, value])
 
     def doutBit(self, value):
-        self.h.write([DBIT_OUT, value])
+        self._hid_device.write([DBIT_OUT, value])
 
-    def tin(self, channel, units = TEMPERATURE):
+    def tin(self, channel, units=TEMPERATURE):
         result = -9999.9
-        self.h.write([TIN, channel, units, 0x0])
+        self._hid_device.write([TIN, channel, units, 0x0])
         d = self._readData(TIN, 5)
         if d:
             reportId = d[0]
@@ -114,10 +115,10 @@ class usb_temp(object):
             result = struct.unpack('f', cs)[0]
         return result
 
-    def tinScan(self, start, end, units = TEMPERATURE):
+    def tinScan(self, start, end, units=TEMPERATURE):
         result = ()
         nchan = 1 + (end - start)
-        self.h.write([TIN_SCAN, start, end, units, 0x0])
+        self._hid_device.write([TIN_SCAN, start, end, units, 0x0])
         d = self._readData(TIN_SCAN, 1 + (4 * nchan))
         if d:
             reportId = d[0]
@@ -133,7 +134,7 @@ class usb_temp(object):
             count = 62
         if (count > 60) and (dtype == 1):
             count = 60
-        self.h.write([MEM_READ, address, dtype, count])
+        self._hid_device.write([MEM_READ, address, dtype, count])
         d = self._readData(MEM_READ, 1 + count)
         if d:
             reportId = d[0]
@@ -145,28 +146,34 @@ class usb_temp(object):
         if address < 0x0100:
             if (count > 59):
                 count = 59
-            self.h.write([MEM_WRITE, address, dtype, count, data])
+            self._hid_device.write([MEM_WRITE, address, dtype, count, data])
             result = True
         return result
 
     def getItem(self, item, sub_item):
-        self.h.write([GET_ITEM, item, sub_item])
+        self._hid_device.write([GET_ITEM, item, sub_item])
         d = self._readData(GET_ITEM)
         if d:
             reportId = d[0]
-            value    = d[1:]
+            value = d[1:]
         return value
 
     def setItem(self, item, sub_item, value):
-        if item > ADC_3:
-            raiseException
-        self.h.write([GET_ITEM, item, sub_item])
+        low = 0
+        high = ADC_3
+        if not 0 <= item <= high:
+            raise Exception("Could not set item {item}"
+                            "(must be greater than or equal to {low}"
+                            "and less than or equal to {high})".format(
+                low=low, high=high
+            ))
+        self._hid_device.write([GET_ITEM, item, sub_item])
 
     def getBurnoutStatus(self, mask):
         result = None
-        self.h.write([GET_BURNOUT_STATUS, 0, 0])
+        self._hid_device.write([GET_BURNOUT_STATUS, 0, 0])
         d = self._readData(GET_BURNOUT_STATUS, 2)
         if d:
             reportId = d[0]
-            result   = d[1] & mask
+            result = d[1] & mask
         return result
