019a3d0a22d33c0ddb0d17b5fbbfd53b230ac21e
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-03-10 14:39:33 -0500

    adding download_url to setup.py

--- setup.py
+++ setup.py
@@ -3,23 +3,22 @@ from setuptools import setup
 from glob import glob
 
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(file_name):
     return open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
 
+VERSION = "0.0.1"
+
 setup(
     name="mcc",
-    version="0.0.1",
+    version=VERSION,
     author="Charles Risicato",
     description=("A library for interacting with the MCC USB-TEMP DAQ"),
     license="GPL3",
     keywords="hardware daq usb hidapi",
     url="https://github.com/TESScience/MCUSBDAQ",
     packages=['mcc'],
+    download_url='https://github.com/TESScience/MCUSBDAQ/tarball/{VERSION}'.format(VERSION=VERSION),
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
